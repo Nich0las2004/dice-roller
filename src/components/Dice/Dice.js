17af8df84523024e945c2ccb940ca03a8d120e36
Nika Oniani <nikaoniani2018@gmail.com>
2023-10-05 22:23:31 +0400

    the dice rotates

--- src/components/Dice/Dice.js
+++ src/components/Dice/Dice.js
@@ -1,6 +1,17 @@
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
 const Dice = () => {
+  const diceRef = useRef();
+
+  useFrame(() => {
+    if (diceRef.current) {
+      diceRef.current.rotation.y += 0.02;
+    }
+  });
+
   return (
-    <mesh>
+    <mesh ref={diceRef}>
       <boxGeometry />
     </mesh>
   );

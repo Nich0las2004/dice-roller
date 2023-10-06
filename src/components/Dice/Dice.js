ab01fde6a9a22ab79bc76fa5f412e30987671beb
Nika Oniani <nikaoniani2018@gmail.com>
2023-10-06 22:54:02 +0400

    added orbit control

--- src/components/Dice/Dice.js
+++ src/components/Dice/Dice.js
@@ -1,18 +1,19 @@
+import { OrbitControls } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 
 const Dice = () => {
   const diceRef = useRef();
 
-  useFrame(() => {
-    if (diceRef.current) {
-      diceRef.current.rotation.y += 0.02;
-    }
-  });
-
   return (
     <mesh ref={diceRef}>
       <boxGeometry />
+      <OrbitControls
+        minDistance={2}
+        maxDistance={6}
+        enableZoom={true}
+        dampingFactor={0.1}
+      />
     </mesh>
   );
 };

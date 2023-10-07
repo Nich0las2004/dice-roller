a39db139c31df5178ff989317b6bdc7f29a36f2e
Nika Oniani <nikaoniani2018@gmail.com>
2023-10-07 08:24:07 +0400

    the dice has 1 dot everywhere

--- src/components/Dice/Dice.js
+++ src/components/Dice/Dice.js
@@ -1,13 +1,19 @@
 import { OrbitControls } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
+import { TextureLoader } from "three";
+
+import dot1 from "../../assets/1-dot.jpg"
 
 const Dice = () => {
   const diceRef = useRef();
 
+  const texture = new TextureLoader().load(dot1);
+
   return (
     <mesh ref={diceRef}>
       <boxGeometry />
+      <meshBasicMaterial map={texture} />
       <OrbitControls
         minDistance={2}
         maxDistance={6}
